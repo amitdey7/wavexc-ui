87f1e0c0ec5cb69149245e6f3df12abdf2a4aa55
shivakumarint <shiva.kumar@integrtr.com>
2024-06-02 19:08:10 +0530

    login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,47 @@
+import React, { useState } from 'react';
 import './Login.css';
+import Logo from '../../assets/images/logo-black.png';
 
 const Login = () => {
-  return <div></div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
+  return (
+    <div className="loginContainer">
+      <div className="login-page">
+        <div className="auth-container">
+          <div className="logo">
+            <img src={Logo} alt="Logo" />
+          </div>
+          <div className="login-form">
+            <p>Login To Your Account</p>
+            <form>
+              <input type="email" placeholder="Email" required />
+              <div className="password-container">
+                <input type={showPassword ? 'text' : 'password'} placeholder="Password" required />
+                <span className="toggle-password" onClick={togglePasswordVisibility}>
+                  <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
+                </span>
+              </div>
+              <div className="options">
+                <a href="#" className="forgot-password">
+                  Forgot password?
+                </a>
+              </div>
+              <div className="buttons">
+                <button type="submit" className="login-button">
+                  Log In
+                </button>
+              </div>
+            </form>
+          </div>
+          <p className="sign-up">
+            Don't have an account? <a href="#">Sign Up</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default Login;
