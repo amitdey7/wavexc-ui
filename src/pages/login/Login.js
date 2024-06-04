ef3c209f707520876cd6d2f49cc276d93bd8fb11
shivakumarint <shiva.kumar@integrtr.com>
2024-06-04 19:06:33 +0530

    fix: login page css fix

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -7,20 +7,37 @@ const Login = () => {
   const togglePasswordVisibility = () => setShowPassword(!showPassword);
 
   return (
-    <div className="loginContainer">
-      <div className="login-page">
-        <div className="auth-container">
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-section">
           <div className="logo">
             <img src={Logo} alt="Logo" />
           </div>
           <div className="login-form">
             <p>Login To Your Account</p>
             <form>
-              <input type="email" placeholder="Email" required />
+              <input
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 w-full"
+                required
+              />
               <div className="password-container">
-                <input type={showPassword ? 'text' : 'password'} placeholder="Password" required />
-                <span className="toggle-password" onClick={togglePasswordVisibility}>
-                  <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
+                <input
+                  id="password"
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Enter your password"
+                  className="px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500 w-full"
+                  required
+                />
+                <span className="toggle-password">
+                  <i
+                    className={`absolute inset-y-1/2 right-3 cursor-pointer text-gray-400 hover:text-gray-600 transition duration-200 ease-in-out ${
+                      showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'
+                    }`}
+                    onClick={togglePasswordVisibility}
+                  ></i>
                 </span>
               </div>
               <div className="options">
